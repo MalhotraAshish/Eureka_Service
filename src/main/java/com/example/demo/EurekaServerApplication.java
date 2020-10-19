9e213a87f4b45d951701f53b11f733c5c8098f99
ltijavab209 <ltijavab209@lti164.iiht.tech>
2020-10-19 18:29:49 +0530

    EurekaServer

--- src/main/java/com/example/demo/EurekaServerApplication.java
+++ src/main/java/com/example/demo/EurekaServerApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 @EnableEurekaServer
 @SpringBootApplication
+
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
